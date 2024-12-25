b73b2382883de6ad31d679106c939a2956c99694
Nikesh Cohen <nikeshcohen@outlook.com>
2024-12-25 16:54:29 +0200

    fix footer link

--- app/_sections/Footer.tsx
+++ app/_sections/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             <a
-              href="https://nikeshcohen.com"
+              href="https://nikeshcohen.dev"
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
